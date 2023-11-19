ff2289e79506d97d3e552097587354d17708b300
juampam <jpablomuralles@gmail.com>
2023-11-18 18:48:37 -0600

    login done

--- bookapp/App.java
+++ bookapp/App.java
@@ -3,7 +3,6 @@ package bookapp;
 public class App {
     public static void main(String[] args) {
         UserController users = new UserController();
-      //  users.createUser("Juampa", "shark", false);
-        users.login("Natalia","bomb");
+        users.login("Juampa","natalia");
     }
 }
--- bookapp/UserController.java
+++ bookapp/UserController.java
@@ -15,27 +15,38 @@ public class UserController {
     public UserController(){
 
     } 
-    // Look for CreateUser in Documentation at https://github.com/juampam/BookLending
-    public void readFile(){
+    // Look for checkers in Documentation at https://github.com/juampam/BookLending
+    public boolean checkUsers(String username){
         String line;
+        boolean exists = false;
         try(BufferedReader br = new BufferedReader(new FileReader(PATH))) {
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
-                rows.add(values);
+               if (values[0].equals(username)) {
+                    exists = true;
+               }
+                // rows.add(values);
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return exists;
     }
     // Look for CreateUser in Documentation at https://github.com/juampam/BookLending
     public void createUser(String username,String password, boolean premium){
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(PATH, true))){
-            writer.write(username + "," + password + "," + premium); //true append
-            writer.newLine();
-        } catch (Exception e) {
-            e.printStackTrace();
+        boolean shit = checkUsers(username);
+        if (shit == false) {
+           try(BufferedWriter writer = new BufferedWriter(new FileWriter(PATH, true))){
+                writer.write(username + "," + password + "," + premium  + "," + 0 ); //true append
+                writer.newLine();
+             } catch (Exception e) {
+                e.printStackTrace();
+             } 
+        }else{
+            System.out.println("The username " + username + " is already taken");
         }
+        
     }
     // Look for Login in Documentation at https://github.com/juampam/BookLending
     public void login(String username, String password){
@@ -43,9 +54,10 @@ public class UserController {
         String userlocated = "";
         try(BufferedReader br = new BufferedReader(new FileReader(PATH))) {
             while ((line = br.readLine()) != null) {
-                if (line.contains(username) && line.contains(password)) {
-                    userlocated = line;    
+                if (line.contains(username)) {
+                    userlocated = line;
                 }
+
             }
 
         } catch (Exception e) {
@@ -57,6 +69,13 @@ public class UserController {
             String[] newEra = userlocated.split(",");
             if (newEra[0].equals(username) && newEra[1].equals(password)) {
                 System.out.println("Wellcome " + username);
+                try(BufferedWriter writer = new BufferedWriter(new FileWriter(".user"))) {
+                    writer.write(username);
+                } catch (Exception e) {
+                     e.printStackTrace();
+                }
+            }else{
+                System.out.println("Incorrect password for " + username );
             }
         }
     }
